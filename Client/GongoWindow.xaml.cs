485a6c7344bace37f4ee24676c7f9b57a5282710
Davidson Bruno <davidson.bruno@ufvjm.edu.br>
2023-01-22 12:50:45 -0300

    Code cleanup

--- Client/GongoWindow.xaml.cs
+++ Client/GongoWindow.xaml.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Gongo.Library;
+using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Web.Script.Serialization;
-using Gongo.Library;
-using System.Net;
 
 namespace Gongo.Client
 {
--- Server/Server.cs
+++ Server/Server.cs
@@ -2,13 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 
 namespace Gongo.Server
